0d8bbfffd756d3c43a7628e73993f499eff79026
WANDOU\YBC <yaoyel@126.com>
2025-07-28 23:13:03 +0800

    删除测试单个用户

--- server/src/routes/wechat.ts
+++ server/src/routes/wechat.ts
@@ -213,11 +213,7 @@ router.all('/callback', async (ctx: Context) => {
           user.createdAt = new Date();
           await userRepository.save(user); 
           console.log('新用户已创建:', user);
-        }
-
-        if (user.id==5 || user.id==8) {
-          user.id=35;
-        }
+        } 
         // 生成JWT token
         const token = JwtUtil.generateToken({ 
           userId: user.id, 
